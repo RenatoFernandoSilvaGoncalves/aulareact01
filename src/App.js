cec3ad7c4db23514e5a266b68a54c14026517bbe
renatofernandosilvagoncalves <renato.goncalves@unoeste.edu.br>
2024-08-19 10:12:34 -0300

    Ciclo de vida: estado do componente

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Data from "./meusComponentes/Data";
 function App() {
   return (
     <div className="App">
-        <Data texto="Data e hora atuais: "/>
-        
+        <Data texto="Data e hora atuais: " timeZone="-3.00"/>
+        <Data texto="Data e hora em Londres: " timeZone="1.00"/>
+        <Data texto="Data e hora em Nova York:" timeZone="-4.00"/>
     </div>
   );
 }
--- src/meusComponentes/Data.js
+++ src/meusComponentes/Data.js
@@ -5,22 +5,42 @@ export default class Data extends Component{
     constructor(props){
         super(); //executa o construtor do pai
         this.props = props;
-        this.dataAtual = new Date().toLocaleString();
+        this.state = {
+                        dataAtual : new Date().toLocaleString()
+                     }       
+    }
+
+    pegaDataDe(timeZone){
+        const dataAtual = new Date();
+        let timeZoneFromDB = parseInt(timeZone); 
+        let diferencaTempo = timeZoneFromDB * 60 + dataAtual.getTimezoneOffset();
+        let milisecundos = parseInt(dataAtual.getTime() + diferencaTempo * 60 * 1000)
+        console.log(milisecundos)
+        return Date(milisecundos);
     }
 
     //fase de montagem
     componentDidMount(){
         console.log("O componente foi montado.");
-        this.dataAtual= new Date().toLocaleString();
+        //Não é permitido atualizar o estado do componente de forma direta
+        //this.state =  ...
+        this.setState({
+           dataAtual: new Date().toLocaleString() 
+        });
     }
 
     componentDidUpdate(){
         console.log("O componente foi atualizado");
+        setTimeout(()=>{
+        this.setState({
+                dataAtual: this.pegaDataDe(this.props.timeZone).toLocaleString() 
+        });
+        },1000);
     }
     //sobrescrita de método
     render(){
         return (
-            <h1>{ this.props.texto || ""}{this.dataAtual}</h1>
+            <h1>{ this.props.texto || ""}{this.state.dataAtual}</h1>
         )
     }
 }
